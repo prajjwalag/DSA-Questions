31f6580a55516a1662f48cbb19a20995c8b4b5e0
Prajjwal Agrahari <50009834+prajjwalag@users.noreply.github.com>
2025-04-30 16:05:32 +0530

    Create If Else Adult Teen Problem.java

---
+++ Basics/Language Basics/If Else Adult Teen Problem.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void isAdult(int age) {
+        if(age>=18) {
+            System.out.print("Adult");
+        } else {
+            System.out.print("Teen");
+        }
+    }
+}
