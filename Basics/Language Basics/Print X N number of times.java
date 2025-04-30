040a8a3de1193c50b981bfd47eb592aeb3268d99
Prajjwal Agrahari <50009834+prajjwalag@users.noreply.github.com>
2025-04-30 16:08:16 +0530

    Create Print X N number of times.java

---
+++ Basics/Language Basics/Print X N number of times.java
@@ -0,0 +1,8 @@
+class Solution {
+    public void printX(int X, int N) {
+        for(int i=0; i < N; i++) {
+            System.out.print(X + " ");
+        }
+        System.out.println();
+    }
+}
