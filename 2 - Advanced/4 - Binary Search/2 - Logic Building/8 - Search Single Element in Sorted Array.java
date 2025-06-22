4af4e2e32716507d0e16b4663bdeb614f229a0fb
Prajjwal <prajjwal.agrahari16@gmail.com>
2025-06-23 01:27:09 +0530

    Solved Search Single Element in Sorted Array

--- 2 - Advanced/4 - Binary Search/2 - Logic Building/8 - Search Single Element in Sorted Array.java
+++ 2 - Advanced/4 - Binary Search/2 - Logic Building/8 - Search Single Element in Sorted Array.java
@@ -1,35 +1,37 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length; // Size of the array.
+        int n = nums.length;
+        int low = 1; //Starting from index 1 because we will compare mid with mid-1, so we need at least one element before mid.
+        int high = n-2; //High is n-2 because we will compare mid with mid+1, so we need at least one element after mid.
+        
+        //The pair will always be at even and odd indices, 
+        //If We are at an even index, then the pair will be at the next index (odd index).
+        //If We are at an odd index, then the pair will be at the previous index (even index).
+        //If the above condition is satisfied, then the single non-duplicate element will be at right half.
+        //If the above condition is not satisfied, then the single non-duplicate element will be at left half.
 
-        // Edge cases:
-        if (n == 1) return nums[0];
-        if (nums[0] != nums[1]) return nums[0];
-        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+        //Eliminate edge cases
+        if(nums.length == 1) { // If the array has only one element, then that is the single non-duplicate element.
+            return nums[0];
+        }
+        if(nums[0] != nums[1]) { // If the first element is not equal to the second element, then the first element is the single non-duplicate element.
+            return nums[0];
+        } 
+        if(nums[n-1] != nums[n-2]) { // If the last element is not equal to the second last element, then the last element is the single non-duplicate element.
+            return nums[n-1];
+        }
 
-        int low = 1, high = n - 2;
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
 
-            // If nums[mid] is the single element:
-            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) { // If mid is not equal to its neighbours, then mid is the single non-duplicate element.
                 return nums[mid];
-            }
-
-            // We are in the left part:
-            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1])
-                || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
-                // Eliminate the left half:
+            } else if(( mid % 2 == 0 && nums[mid] == nums[mid + 1] ) || (mid % 2 == 1) && nums[mid] == nums[mid-1]) { // If mid is even and mid is equal to mid+1, or if mid is odd and mid is equal to mid-1, then the single non-duplicate element is in the right half.
                 low = mid + 1;
-            }
-            // We are in the right part:
-            else {
-                // Eliminate the right half:
+            } else {
                 high = mid - 1;
             }
         }
-
-        // Dummy return statement:
         return -1;
     }
 }
