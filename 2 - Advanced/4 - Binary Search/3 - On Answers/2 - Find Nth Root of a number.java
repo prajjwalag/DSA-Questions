b938ee3745a48d12b6ec23644583f16376657953
Prajjwal <prajjwal.agrahari16@gmail.com>
2025-06-24 23:34:02 +0530

    Solved Find Nth Root of a number

--- 2 - Advanced/4 - Binary Search/3 - On Answers/2 - Find Nth Root of a number.java
+++ 2 - Advanced/4 - Binary Search/3 - On Answers/2 - Find Nth Root of a number.java
@@ -19,3 +19,37 @@ class Solution {
         return -1;
     }
 }
+
+//Aproach 2
+class Solution {
+    public long calculateNthPower(int num, int power, int target) {
+      long ans = 1;
+       while(power > 0) {
+          ans *= num;
+          if(ans > target) {
+            return ans;
+          }
+          power--;
+       }
+       return ans;
+    }
+    public int NthRoot(int N, int M) {
+        int low = 1;
+        int high = M;
+
+        while(low <= high) {
+           int mid = low + (high - low)/2;
+
+           long value = calculateNthPower(mid, N, M);
+
+           if(value == M) {
+              return mid;
+           } else if(value < M) {
+              low = mid + 1;
+           } else {
+             high = mid -1;
+           }
+        }
+        return -1;
+    }
+}
