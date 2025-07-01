e08651acaab4dae3d1de5a1d453f52134e98c803
Prajjwal <prajjwal.agrahari16@gmail.com>
2025-07-01 23:00:03 +0530

    Solved Peak Element

--- 2 - Advanced/4 - Binary Search/4 - FAQs/3 - Find Peak Element.java
+++ 2 - Advanced/4 - Binary Search/4 - FAQs/3 - Find Peak Element.java
@@ -1,36 +1,26 @@
-import java.util.*;
-
 class Solution {
-    // Function to find peak element in the array
     public int findPeakElement(int[] arr) {
-        // Size of array
         int n = arr.length;
-        
-        /* Iterate through the array
-           to find the peak element */
-        for (int i = 0; i < n; i++) {
-            
-            // Check if arr[i] is a peak
-            if ((i == 0 || arr[i - 1] < arr[i]) && (i == n - 1 || arr[i] > arr[i + 1])) {
-                
-                // Return the index of peak element
-                return i;
-            }
-        }
-        /* Return -1 if no peak element
-           found (dummy return) */
-        return -1;
-    }
+        int low = 1;
+        int high = n-2;
 
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 5, 1};
-        
-        // Create an instance of the Solution class
-        Solution sol = new Solution();
-        
-        int ans = sol.findPeakElement(arr);
-        
-        // Output the result
-        System.out.println("The peak is at index: " + ans);
+        if (n == 1) return 0;
+        if (arr[0] > arr[1]) return 0;
+        if (arr[n - 1] > arr[n - 2]) return n - 1;
+
+        while(low <= high) {
+          int mid = low + (high - low)/2;
+
+          if(arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]) {
+            return mid;
+          } else if(arr[mid-1] < arr[mid] && arr[mid] < arr[mid+1]) {
+            
+            low = mid + 1;
+          } else {
+            high = mid - 1;
+          }
+        }
+        return 0;
     }
+    
 }
