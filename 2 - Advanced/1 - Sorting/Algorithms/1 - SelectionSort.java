a8ec2dae368f7d741b8f7814be572417a486d8c3
Prajjwal Agrahari <50009834+prajjwalag@users.noreply.github.com>
2025-09-18 15:16:31 +0530

    Update 1 - SelectionSort.java

--- 2 - Advanced/1 - Sorting/Algorithms/1 - SelectionSort.java
+++ 2 - Advanced/1 - Sorting/Algorithms/1 - SelectionSort.java
@@ -1,17 +1,29 @@
+/*
+Intuition: 
+1. Iterate through Array and find the minimum element every time.
+2. In each iteration, consider that minimum element is the current element.
+3. In a nested loop, find the minimum element.
+4. If minimum Element index is not the current index, then swap the minimum element with the current element.
+5. By repeating this, atlast there will only 1 element left, which can not be compared from any other element
+    and it will be the largest element. Hence, we can leave the last element, that's why we are iterating to n-1 only.
+*/
+
+
 class Solution {
     public int[] selectionSort(int[] nums) {
-        for(int i = 0; i<nums.length-1; i++) {
+        for(int i = 0; i < nums.length - 1; i++) {
             int minIndex = i;
-            int minEle = nums[i];
             for(int j = i+1; j < nums.length; j++) {
-                if(nums[j] < minEle) {
-                    minEle = nums[j];
+                if(nums[j] < nums[minIndex]) {
                     minIndex = j;
                 }
             }
-            int temp = nums[i];
-            nums[i] = minEle;
-            nums[minIndex] = temp;
+
+            if(minIndex != i) {
+                int temp = nums[minIndex];
+                nums[minIndex] = nums[i];
+                nums[i] = temp;
+            }
         }
         return nums;
     }
