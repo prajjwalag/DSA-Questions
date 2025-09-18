81610118917220e472f60250fc92d382e2af0726
Prajjwal Agrahari <50009834+prajjwalag@users.noreply.github.com>
2025-09-18 15:27:47 +0530

    Update 2 - BubbleSort.java

--- 2 - Advanced/1 - Sorting/Algorithms/2 - BubbleSort.java
+++ 2 - Advanced/1 - Sorting/Algorithms/2 - BubbleSort.java
@@ -1,16 +1,29 @@
+/*
+Intuition:
+
+Iterate through the array and compare two consecutive elements, if the ith element is greater than
+i+1th element, then swap both of them
+
+1. Run a loop from 0 to n-1.
+2. Run a nested loop, from 0 to n - i - 1, because in each iteration, the largest element will be placed at the last index
+    So, we do not need to compare in the sorted area.
+3. Check if j > j+1, if true then swap it.
+4. At last, check if any element is swapped or not, if not swapped, then that means, array is already sortedm hence break the loop.
+*/
+
 class Solution {
     public int[] bubbleSort(int[] nums) {
-        int swapCount = 0;
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = 0; j < nums.length-1; j++) {
+        for(int i = 0; i < nums.length - 1; i++) {
+            boolean isSwapped = false;
+            for(int j = 0; j < nums.length - i - 1; j++) {
                 if(nums[j] > nums[j+1]) {
                     int temp = nums[j];
                     nums[j] = nums[j+1];
-                    nums[j+1] = temp; 
-                    swapCount++;
+                    nums[j+1] = temp;
+                    isSwapped = true;
                 }
             }
-            if(swapCount==0){
+            if(!isSwapped) {
                 break;
             }
         }
