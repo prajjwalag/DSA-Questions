8870a13eb1f0aacfe64bd0402f2769300d56ce82
Prajjwal Agrahari <50009834+prajjwalag@users.noreply.github.com>
2025-09-19 23:06:48 +0530

    Update 4 - MergeSort.java

--- 2 - Advanced/1 - Sorting/Algorithms/4 - MergeSort.java
+++ 2 - Advanced/1 - Sorting/Algorithms/4 - MergeSort.java
@@ -1,44 +1,51 @@
+
+
+
 class Solution {
-    public int[] mergeSort(int[] nums) {
-        int low = 0;
-        int high = nums.length-1;
-        mergeSorting(nums, low, high);
-        return nums;
-    }
-    public void mergeSorting(int[] nums, int low, int high) {
-        
-        if(low>=high) {
-            return;
-        }
-        
-        int mid = low+(high-low)/2;
-        mergeSorting(nums, low, mid);
-        mergeSorting(nums, mid+1, high);
-        merge(nums, low, mid, high);
-    }
     public void merge(int[] nums, int low, int mid, int high) {
-        ArrayList<Integer> tempArr = new ArrayList<>();
         int left = low;
         int right = mid+1;
+        List<Integer> array = new ArrayList<>();
+
         while(left <= mid && right <= high) {
             if(nums[left] <= nums[right]) {
-                tempArr.add(nums[left]);
+                array.add(nums[left]);
                 left++;
             } else {
-                tempArr.add(nums[right]);
+                array.add(nums[right]);
                 right++;
             }
         }
+
         while(left <= mid) {
-            tempArr.add(nums[left]);
+            array.add(nums[left]);
             left++;
         }
+
         while(right <= high) {
-            tempArr.add(nums[right]);
+            array.add(nums[right]);
             right++;
         }
-        for(int i = low; i<=high; i++) {
-            nums[i] = tempArr.get(i-low);
+
+        int index = 0;
+        for(int i = low; i <= high; i++ ) {
+            nums[i] = array.get(index++);
         }
+    }
+    public void mergeSortHelper(int[] nums, int low, int high) {
+        if(low >= high) {
+            return;
+        }
+        
+        int mid = low + (high-low)/2;
+
+        mergeSortHelper(nums, low, mid);
+        mergeSortHelper(nums, mid+1, high);
+        merge(nums, low, mid, high);
+    }
+    public int[] mergeSort(int[] nums) {
+        int n = nums.length-1;
+        mergeSortHelper(nums, 0, n);
+        return nums;
     }
 }
