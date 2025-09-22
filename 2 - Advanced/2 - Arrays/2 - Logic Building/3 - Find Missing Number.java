ea1c81d014747884cbcc2a4f1148232f6f046b97
Prajjwal Agrahari <50009834+prajjwalag@users.noreply.github.com>
2025-09-22 22:37:40 +0530

    Update 3 - Find Missing Number.java

--- 2 - Advanced/2 - Arrays/2 - Logic Building/3 - Find Missing Number.java
+++ 2 - Advanced/2 - Arrays/2 - Logic Building/3 - Find Missing Number.java
@@ -1,3 +1,12 @@
+
+/*
+Approach 1:
+1. Initialize two variables xor1, xor2 as 0. xor1 variable will calculate the XOR of 1 to N
+2. Calculate the XOR of all the elements in the array by xor2 = xor2 ^ arr[i]..
+3. Finally, the answer will be the XOR of xor1 and xor2.
+*/
+
+
 class Solution1UsingSumofNNumbers {
     public int missingNumber(int[] nums) {
         int size = nums.length;
@@ -11,16 +20,24 @@ class Solution1UsingSumofNNumbers {
     }
 }
 
+
+/*
+Approach 2 :
+
+1. Initialize two variables xor1, xor2 as 0. xor1 variable will calculate the XOR of 1 to N
+2. Calculate the XOR of all the elements in the array by xor2 = xor2 ^ arr[i]..
+3. Finally, the answer will be the XOR of xor1 and xor2.
+*/
 class Solution2UsingXor {
     public int missingNumber(int[] nums) {
-        int size = nums.length;
-        int expectedXorValue = 0;
-        for(int  i = 1; i <= size; i++) {
-            expectedXorValue^=i;
+        int n = nums.length;
+        int expectedXor = 0;
+        int actualXor = 0;
+        for(int i = 0; i < n; i++ ) {
+          expectedXor ^= i+1;
+          actualXor ^= nums[i];
         }
-        for(int i = 0; i < size; i++) {
-            expectedXorValue ^= nums[i];
-        }
-        return expectedXorValue;
+        int missingXor = actualXor ^ expectedXor;
+        return missingXor;
     }
 }
